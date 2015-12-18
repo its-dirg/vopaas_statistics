71dd35ef396bc940b97f9cb341ba8eea5c82b668
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-12-18 10:07:12 +0100

    Bump version: 0.0.1 → 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vopaas_statistics',
-    version='0.0.1',
+    version='1.0.0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/its-dirg/vopaas_statistics',
